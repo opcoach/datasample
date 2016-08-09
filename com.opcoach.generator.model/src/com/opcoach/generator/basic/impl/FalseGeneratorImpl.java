ac9af32f6a33039f558bb4f4b43273a58f463729
Olivier Prouvost <olivier.prouvost@opcoach.com>
2016-08-10 00:01:29 +0200

    True and False Generato
    Change-Id: I0989a4e422ca1a7702b0887722fa1f0246dd2ddb
    Signed-off-by: Olivier Prouvost <olivier.prouvost@opcoach.com>

--- com.opcoach.generator.model/src/com/opcoach/generator/basic/impl/FalseGeneratorImpl.java
+++ com.opcoach.generator.model/src/com/opcoach/generator/basic/impl/FalseGeneratorImpl.java
@@ -5,5 +5,15 @@ import com.opcoach.generator.basic.FalseGenerator;
 // This class overrides the generated class and will be instantiated by factory
 public class FalseGeneratorImpl extends MFalseGeneratorImpl implements FalseGenerator
 {
-
+	@Override
+	protected Boolean generateRandomValue()
+	{
+		return Boolean.FALSE;
+	}
+	
+	@Override
+	protected Boolean generateSimpleValue()
+	{
+		return Boolean.FALSE;
+	}
 }
--- com.opcoach.generator.model/src/com/opcoach/generator/basic/impl/TrueGeneratorImpl.java
+++ com.opcoach.generator.model/src/com/opcoach/generator/basic/impl/TrueGeneratorImpl.java
@@ -5,5 +5,17 @@ import com.opcoach.generator.basic.TrueGenerator;
 // This class overrides the generated class and will be instantiated by factory
 public class TrueGeneratorImpl extends MTrueGeneratorImpl implements TrueGenerator
 {
+	@Override
+	protected Boolean generateRandomValue()
+	{
+		return Boolean.TRUE;
+	}
+	
+	@Override
+	protected Boolean generateSimpleValue()
+	{
+		return Boolean.TRUE;
+	}
+
 
 }
