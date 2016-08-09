4ee2b4e17b7ec7366d208b579f7ec12929b25d87
Olivier Prouvost <olivier.prouvost@opcoach.com>
2016-08-10 00:06:47 +0200

    NullValueGenerator
    Change-Id: I173902db45f997cb9cf6c9223edb77026f3f3f19
    Signed-off-by: Olivier Prouvost <olivier.prouvost@opcoach.com>

--- com.opcoach.generator.model/src/com/opcoach/generator/basic/impl/NullValueGeneratorImpl.java
+++ com.opcoach.generator.model/src/com/opcoach/generator/basic/impl/NullValueGeneratorImpl.java
@@ -5,5 +5,16 @@ import com.opcoach.generator.basic.NullValueGenerator;
 // This class overrides the generated class and will be instantiated by factory
 public class NullValueGeneratorImpl extends MNullValueGeneratorImpl implements NullValueGenerator
 {
+	@Override
+	protected Object generateRandomValue()
+	{
+		return null;
+	}
 
+
+	@Override
+	protected Object generateSimpleValue()
+	{
+		return null;
+	}
 }
