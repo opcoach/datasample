086cb2e863735024316a5727b9791155fc54f01a
Olivier Prouvost <olivier.prouvost@opcoach.com>
2016-08-10 00:04:01 +0200

    IDGenerator
    Change-Id: Id46f14b2eddf1d79462b9bb273c9eac6affc52bd
    Signed-off-by: Olivier Prouvost <olivier.prouvost@opcoach.com>

--- com.opcoach.generator.model/src/com/opcoach/generator/basic/impl/IDGeneratorImpl.java
+++ com.opcoach.generator.model/src/com/opcoach/generator/basic/impl/IDGeneratorImpl.java
@@ -5,5 +5,56 @@ import com.opcoach.generator.basic.IDGenerator;
 // This class overrides the generated class and will be instantiated by factory
 public class IDGeneratorImpl extends MIDGeneratorImpl implements IDGenerator
 {
+	   /**  The last key returned */
+ private static long __lastKey = 0L;
+
+ /** The time that represents zero. */
+ private static long __startTime = System.currentTimeMillis();
+
+	protected static final String PREFIX_EDEFAULT = "";
+
+	@Override
+	protected String generateRandomValue()
+	{
+		// No random value
+		return generateSimpleValue();
+	}
+
+
+
+	@Override
+	protected String generateSimpleValue()
+	{
+		/** Generate a value base 36 encoded on 10 letters  */
+        long value;
+        // Find a new value
+        synchronized (IDGenerator.class)
+        {
+            while ((value = System.currentTimeMillis() - __startTime) <= __lastKey)
+            {
+                // iterate
+            }
+            __lastKey = value;
+        }
+
+        // Convert it to a string using radix 36 (more compact)
+        String longString = Long.toString(value, Character.MAX_RADIX);
+    
+        // And pad it right with zeroes according to the key length so that
+        // alphabetical order is also numeric order.
+        int longLength = longString.length() + prefix.length();
+
+        StringBuffer result = new StringBuffer(prefix);
+        for (int i = keyLength - longLength; i > 0; i--)
+        {
+            result.append('0');
+        }
+        result.append(longString);
+
+        // Done
+        return result.toString();
+    
+	}
+
 
 }
