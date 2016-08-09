b84f844007086623cedf7fcb85cfe6f6ed63736b
Olivier Prouvost <olivier.prouvost@opcoach.com>
2016-08-10 00:00:22 +0200

    Migrate DoubleGeneratorImpl
    Change-Id: I0645b56d5e040ebdf580866198ba190efbfa6752
    Signed-off-by: Olivier Prouvost <olivier.prouvost@opcoach.com>

--- com.opcoach.generator.model/src/com/opcoach/generator/basic/impl/DoubleGeneratorImpl.java
+++ com.opcoach.generator.model/src/com/opcoach/generator/basic/impl/DoubleGeneratorImpl.java
@@ -5,5 +5,62 @@ import com.opcoach.generator.basic.DoubleGenerator;
 // This class overrides the generated class and will be instantiated by factory
 public class DoubleGeneratorImpl extends MDoubleGeneratorImpl implements DoubleGenerator
 {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated NOT
+	 */
+	protected DoubleGeneratorImpl()
+	{
+		this(0.0d, 100.0d);
+	}
+	
+		
+    /**  Build it with low and high bounds */
+	public DoubleGeneratorImpl(Double vlow, Double vhigh) 
+	{
+		super();
+		setLow(vlow);
+		setHigh(vhigh);
+		step = 1.0d;
+	}
+	
 
+	@Override
+	protected Double generateRandomValue()
+	{
+		 // Generate random value.
+	   	  double result = getRandomizer().nextDouble();
+	   	  if (result > (high - low))
+	   	  {
+	   		  // Must adjust the value. 
+	   		  result = (double) (result - 0.5d);
+	   	  }
+	   	  // Can return the result .
+	   	  return low + result;
+
+	}
+
+
+
+	@Override
+	protected Double generateSimpleValue()
+	{
+		double result;
+		
+		if (lastGeneratedValue == null)
+			result = low;
+		else
+		{
+			result =  lastGeneratedValue + step;
+			// Restart � low value if value to high.
+			if (result > high)
+				result = low + (result - high);
+		}
+		
+		return result;
+	}
+
+	
+	
 }
