4286c3e79c1cdbc8f473acf431cf0e8fa422626b
Olivier Prouvost <olivier.prouvost@opcoach.com>
2016-08-10 00:06:12 +0200

    Long Generator
    Change-Id: I9ab2eec65dd4343cd4902dc4d43d8fdafbb78d57
    Signed-off-by: Olivier Prouvost <olivier.prouvost@opcoach.com>

--- com.opcoach.generator.model/src/com/opcoach/generator/basic/impl/LongGeneratorImpl.java
+++ com.opcoach.generator.model/src/com/opcoach/generator/basic/impl/LongGeneratorImpl.java
@@ -5,5 +5,49 @@ import com.opcoach.generator.basic.LongGenerator;
 // This class overrides the generated class and will be instantiated by factory
 public class LongGeneratorImpl extends MLongGeneratorImpl implements LongGenerator
 {
+	protected LongGeneratorImpl()
+	{
+		this(0L, 100000L);
+	}
+	
+	/** Build it with low and high bounds */
+	public LongGeneratorImpl( Long vlow, Long vhigh)
+	{
+		setLow(vlow);
+		setHigh(vhigh);
+		step = 1L;
+	}
+
+
+	@Override
+	protected Long generateRandomValue()
+	{
+		// Generate random value. Get a value between O,1 and compute value inside bounds
+		double v = getRandomizer().nextDouble();
+
+		// Must adjust the value.
+		long result = low + (long) (v * (high - low));
+
+		// Can return the result .
+		return result;
+	}
+
+	@Override
+	protected Long generateSimpleValue()
+	{
+		long result;
+
+		if (lastGeneratedValue == null)
+			result = low;
+		else
+		{
+			result = lastGeneratedValue + step;
+			// Restart � low value if value to high.
+			if (result > high)
+				result = low + (result - high);
+		}
+
+		return result;
+	}
 
 }
