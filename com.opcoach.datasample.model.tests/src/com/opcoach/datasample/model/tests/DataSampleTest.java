fe105f06a85504891c3463cca1a55cf9c7165786
Olivier Prouvost <olivier.prouvost@opcoach.com>
2018-07-29 11:07:13 +0200

    Tycho build ok with tests
    Change-Id: Ie938ffa0c5965f68e1d52c91298ded27226fb9fc
    Signed-off-by: Olivier Prouvost <olivier.prouvost@opcoach.com>

--- com.opcoach.datasample.model.tests/src/com/opcoach/datasample/model/tests/DataSampleTest.java
+++ com.opcoach.datasample.model.tests/src/com/opcoach/datasample/model/tests/DataSampleTest.java
@@ -1,24 +1,25 @@
 package com.opcoach.datasample.model.tests;
 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EcorePackage;
+import org.junit.Test;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
 import com.opcoach.datasample.DataSample;
 import com.opcoach.datasample.DatasampleFactory;
 import com.opcoach.datasample.EntityGenerator;
 
-class DataSampleTest extends DataSampleUtilTest {
+class DataSampleTest extends DataSampleUtilTest  // extends to launch all tests together (bug in eclipse launch config)
+{
 	
 	private static DataSample dataSample = null;
 	private static EntityGenerator rootGen = null;
@@ -57,29 +58,47 @@ class DataSampleTest extends DataSampleUtilTest {
 	void testGetPackage() {
 		// EPackage must be the same than ECorePackage
 		EPackage p = dataSample.getPackage();
+		
+		/** JUNIT 5 Ready
 		assertNotNull(p, "Package can not be null");
 		assertEquals(p, EcorePackage.eINSTANCE, "Package must be Ecore Package");
+		*/
+		
+		assertNotNull( "Package can not be null", p);
+		assertEquals( "Package must be Ecore Package", p, EcorePackage.eINSTANCE);
 	}
 
 	@Test
 	@DisplayName("Root generation EClass must be EPackage")
 	void testGetRootEntity() {
 		EClassifier c = dataSample.getRootEntity();
-		assertEquals(c, EcorePackage.Literals.EPACKAGE, "Root entity must be the EPackage Eclass");
+		
+		// JUNIT 5
+		// assertEquals(c, EcorePackage.Literals.EPACKAGE, "Root entity must be the EPackage Eclass");
+		
+		 assertEquals("Root entity must be the EPackage Eclass", c, EcorePackage.Literals.EPACKAGE);
 	}
 	
 	@Test
 	@DisplayName("Entity generator must be set on EPackage")
 	void testGetEClassInMainEntityGenerator() {
 		EClassifier gc = dataSample.getEntityGenerators().get(0).getEntity();
-		assertEquals(gc, EcorePackage.Literals.EPACKAGE, "EClass in first main generator must be set on EPackage");
+		// JUNIT 5 Ready 
+		// assertEquals(gc, EcorePackage.Literals.EPACKAGE, "EClass in first main generator must be set on EPackage");
+		
+		assertEquals("EClass in first main generator must be set on EPackage", gc, EcorePackage.Literals.EPACKAGE);
+
+		
 	}
 
 	@Test
 	@DisplayName("Entity generator inside packageGenerator must be set on EClass")
 	void testGetEClassInPackageGenerator() {
 		EClassifier gc = dataSample.getEntityGenerators().get(0).getChildGenerators().get(0).getEntity();
-		assertEquals(gc, EcorePackage.Literals.ECLASS, "EClass in first main generator must be set on EPackage");
+		
+		// JUNIT 5 Ready 
+		// assertEquals(gc, EcorePackage.Literals.ECLASS, "EClass in first main generator must be set on EPackage");
+		assertEquals("EClass in first main generator must be set on EPackage", gc, EcorePackage.Literals.ECLASS);
 	}
 
 	@Test
--- com.opcoach.datasample.model.tests/src/com/opcoach/datasample/model/tests/DataSampleUtilTest.java
+++ com.opcoach.datasample.model.tests/src/com/opcoach/datasample/model/tests/DataSampleUtilTest.java
@@ -1,13 +1,18 @@
 package com.opcoach.datasample.model.tests;
 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Set;
 
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EcorePackage;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,7 +20,7 @@ import com.opcoach.datasample.DataSampleUtil;
 
 class DataSampleUtilTest {
 
-/*	@BeforeAll
+	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 	}
 
@@ -30,7 +35,7 @@ class DataSampleUtilTest {
 	@AfterEach
 	void tearDown() throws Exception {
 	}
-	*/
+	
 
 	@Test
 	@DisplayName("Find the subclasses of EClassifier")
@@ -38,9 +43,14 @@ class DataSampleUtilTest {
 		Set<EClass> subclasses = DataSampleUtil.getSubClasses(EcorePackage.Literals.ECLASSIFIER);
 
 		// Must find EClass, EDataType and EEnum
-		assertTrue(subclasses.contains(EcorePackage.Literals.ECLASS), "EClass must be found as subclass of EClassifier" );
+		/* JUNIT 5 Ready
+	     assertTrue(subclasses.contains(EcorePackage.Literals.ECLASS), "EClass must be found as subclass of EClassifier" );
 		assertTrue(subclasses.contains(EcorePackage.Literals.EDATA_TYPE), "EDataType must be found as subclass of EClassifier");
 		assertTrue(subclasses.contains(EcorePackage.Literals.EENUM), "EEnum must be found as subclass of EClassifier");
+		*/
+		assertTrue( "EClass must be found as subclass of EClassifier" , subclasses.contains(EcorePackage.Literals.ECLASS));
+		assertTrue("EDataType must be found as subclass of EClassifier", subclasses.contains(EcorePackage.Literals.EDATA_TYPE));
+		assertTrue("EEnum must be found as subclass of EClassifier", subclasses.contains(EcorePackage.Literals.EENUM) );
 		
 	}
 
@@ -50,8 +60,9 @@ class DataSampleUtilTest {
 		Set<EClass> subclasses = DataSampleUtil.getSubClasses(EcorePackage.Literals.EMODEL_ELEMENT);
 
 		// There must be 16 subclasses of model element	
-		assertEquals(subclasses.size(), 16, "There must be 16 subclasses of EModelElement in EcorePackage");
-		
+		// JUNIT5 Ready assertEquals(subclasses.size(), 16, "There must be 16 subclasses of EModelElement in EcorePackage");
+		assertEquals("There must be 16 subclasses of EModelElement in EcorePackage", subclasses.size(), 16);
+
 	}
 	
 	@Test
@@ -59,16 +70,17 @@ class DataSampleUtilTest {
 	void testGetSubClassesOfEStructuralFeature() {
 		Set<EClass> subclasses = DataSampleUtil.getSubClasses(EcorePackage.Literals.ESTRUCTURAL_FEATURE);
 				
-		assertTrue(subclasses.contains(EcorePackage.Literals.EATTRIBUTE), "EAttribute must be found as subclass of EStructuralFeature");
+	/*  JUNIT 5 Ready
+	 	assertTrue(subclasses.contains(EcorePackage.Literals.EATTRIBUTE), "EAttribute must be found as subclass of EStructuralFeature");
 		assertTrue(subclasses.contains(EcorePackage.Literals.EREFERENCE), "EReference must be found as subclass of EStructuralFeature");
+		*/
+		assertTrue("EAttribute must be found as subclass of EStructuralFeature", subclasses.contains(EcorePackage.Literals.EATTRIBUTE));
+		assertTrue("EReference must be found as subclass of EStructuralFeature", subclasses.contains(EcorePackage.Literals.EREFERENCE));
 		
+
 	}
 	
-	/*@Test
-	void testGetSortedClasses() {
-		List<EClassifier> subclasses = DataSampleUtil.getSortedClasses(EcorePackage.eINSTANCE);
-				
-	} */
+	
 	
 	@Test
 	@DisplayName("Test children classes expected in EPackage")
@@ -78,6 +90,7 @@ class DataSampleUtilTest {
 		// An EPackage must set : EClassifiers, EPackages
 		childrenTypes.forEach((c) -> System.out.println("In EPackage, found this class  " + c.getName()));
 		
+		/*  JUNIT 5 Ready 
 		assertEquals(childrenTypes.size(), 5, "There must be 5 children concrete EClass for EPackage ");
 		
 		assertTrue(childrenTypes.contains(EcorePackage.Literals.ECLASS), "EClass is contained in a EPackage");
@@ -85,8 +98,15 @@ class DataSampleUtilTest {
 		assertTrue(childrenTypes.contains(EcorePackage.Literals.EENUM), "EEnum is contained in a EPackage");
 		assertTrue(childrenTypes.contains(EcorePackage.Literals.EPACKAGE), "EPackage is contained in a EPackage");
 		assertTrue(childrenTypes.contains(EcorePackage.Literals.EANNOTATION), "EAnnotation is contained in a EPackage");
-
-				
+*/
+	assertEquals("There must be 5 children concrete EClass for EPackage ", childrenTypes.size(), 5);
+		
+		assertTrue("EClass is contained in a EPackage", childrenTypes.contains(EcorePackage.Literals.ECLASS));
+		assertTrue("DataType is contained in a EPackage", childrenTypes.contains(EcorePackage.Literals.EDATA_TYPE));
+		assertTrue("EEnum is contained in a EPackage", childrenTypes.contains(EcorePackage.Literals.EENUM));
+		assertTrue("EPackage is contained in a EPackage", childrenTypes.contains(EcorePackage.Literals.EPACKAGE));
+		assertTrue("EAnnotation is contained in a EPackage", childrenTypes.contains(EcorePackage.Literals.EANNOTATION));
+		
 	}
 	
 	@Test
@@ -97,6 +117,7 @@ class DataSampleUtilTest {
 		// An EClass must contains attributes, types, operations... 
 		childrenTypes.forEach((c) -> System.out.println("In EClass, found this class  " + c.getName()));
 		
+		/*  JUNIT 5 Ready
 		assertEquals(childrenTypes.size(), 6, "There must be 6 children concrete EClass for EClass ");
 		
 		assertTrue(childrenTypes.contains(EcorePackage.Literals.EATTRIBUTE), "EAttribute is contained in a EClass");
@@ -105,8 +126,16 @@ class DataSampleUtilTest {
 		assertTrue(childrenTypes.contains(EcorePackage.Literals.EANNOTATION), "EAnnotation is contained in a EClass");
 		assertTrue(childrenTypes.contains(EcorePackage.Literals.ETYPE_PARAMETER),"ETypeParameter is contained in a EClass");
 		assertTrue(childrenTypes.contains(EcorePackage.Literals.EGENERIC_TYPE), "EGenericType is contained in a EClass");
+*/
+		assertEquals("There must be 6 children concrete EClass for EClass ", childrenTypes.size(), 6);
+		
+		assertTrue("EAttribute is contained in a EClass", childrenTypes.contains(EcorePackage.Literals.EATTRIBUTE));
+		assertTrue("EReference is contained in a EClass", childrenTypes.contains(EcorePackage.Literals.EREFERENCE));
+		assertTrue("EOperation is contained in a EClass", childrenTypes.contains(EcorePackage.Literals.EOPERATION));
+		assertTrue("EAnnotation is contained in a EClass", childrenTypes.contains(EcorePackage.Literals.EANNOTATION));
+		assertTrue("ETypeParameter is contained in a EClass", childrenTypes.contains(EcorePackage.Literals.ETYPE_PARAMETER));
+		assertTrue("EGenericType is contained in a EClass", childrenTypes.contains(EcorePackage.Literals.EGENERIC_TYPE));
 
-				
 	}
 	
 	@Test
@@ -117,13 +146,22 @@ class DataSampleUtilTest {
 		// An EPackage must set : EClassifiers, EPackages
 		childrenTypes.forEach((c) -> System.out.println("In EOperation, found this class  " + c.getName()));
 		
+		/*  JUNIT 5 Ready
 		assertEquals(childrenTypes.size(), 4, "There must be 4 child concrete EClass for EOperation ");
 		
 		assertTrue(childrenTypes.contains(EcorePackage.Literals.EPARAMETER), "EParameter is contained in a EOPeration");
 		assertTrue(childrenTypes.contains(EcorePackage.Literals.EANNOTATION), "EAnnotation is contained in a EClass");
 		assertTrue(childrenTypes.contains(EcorePackage.Literals.ETYPE_PARAMETER), "ETypeParameter is contained in a EClass");
 		assertTrue(childrenTypes.contains(EcorePackage.Literals.EGENERIC_TYPE), "EGenericType is contained in a EClass");
-	
+	*/
+		
+		assertEquals("There must be 4 child concrete EClass for EOperation ", childrenTypes.size(), 4);
+		
+		assertTrue("EParameter is contained in a EOPeration", childrenTypes.contains(EcorePackage.Literals.EPARAMETER));
+		assertTrue("EAnnotation is contained in a EClass", childrenTypes.contains(EcorePackage.Literals.EANNOTATION));
+		assertTrue("ETypeParameter is contained in a EClass", childrenTypes.contains(EcorePackage.Literals.ETYPE_PARAMETER));
+		assertTrue("EGenericType is contained in a EClass", childrenTypes.contains(EcorePackage.Literals.EGENERIC_TYPE));
+
 	}
 	
 
